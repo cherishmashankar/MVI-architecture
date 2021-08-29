f7d263da7b43c938a854c6d35724714fcf4071cb
unknown <cherry.sh1996@gmail.com>
2021-08-29 09:39:40 +0200

    fragment and menu added

--- app/src/main/java/com/example/android/mvi/MainActivity.kt
+++ app/src/main/java/com/example/android/mvi/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.android.mvi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showMainFragment()
+
+    }
+
+    fun showMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MainFragment(),"MainFragment")
+            .commit()
+
     }
 }
