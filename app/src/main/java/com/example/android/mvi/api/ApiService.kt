b010dd93b5bf3306ade876849834dac7e591330a
unknown <cherry.sh1996@gmail.com>
2021-09-01 08:08:35 +0200

    LiveDataCallAdapterFactory and ApiResponses added

--- app/src/main/java/com/example/android/mvi/api/ApiService.kt
+++ app/src/main/java/com/example/android/mvi/api/ApiService.kt
@@ -1,7 +1,9 @@
 package com.example.android.mvi.api
 
+import androidx.lifecycle.LiveData
 import com.example.android.mvi.model.Blogs
 import com.example.android.mvi.model.User
+import com.example.android.mvi.util.GenericApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -10,9 +12,9 @@ interface ApiService {
     @GET("/placeholder/user/{userId}")
     fun getUser(
         @Path("userId") userId: String
-    ): User
+    ): LiveData<GenericApiResponse<User>>
 
     @GET("/placeholder/blogs")
     fun getBlogPosts(
-    ): List<Blogs>
+    ): LiveData<GenericApiResponse<List<Blogs>>>
 }
--- app/src/main/java/com/example/android/mvi/api/RetrofitBuilder.kt
+++ app/src/main/java/com/example/android/mvi/api/RetrofitBuilder.kt
@@ -1,5 +1,6 @@
 package com.example.android.mvi.api
 
+import com.example.android.mvi.util.LiveDataCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,6 +13,7 @@ object RetrofitBuilder {
     val retrofitBuilder: Retrofit.Builder by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .addCallAdapterFactory(LiveDataCallAdapterFactory())
             .addConverterFactory(GsonConverterFactory.create())
     }
 
